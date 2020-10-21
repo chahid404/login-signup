e2c004e98d200f8dc778ce1fc83b5533a6c75939
chahid chirchi <chahid23chirchi@gmail.com>
2020-10-21 23:14:38 +0100

    detaildialog

--- react_version/react/demo1/src/app/modules/ECommerce/pages/customers/customer-delete-dialog/CustomerDeleteDialog.js
+++ react_version/react/demo1/src/app/modules/ECommerce/pages/customers/customer-delete-dialog/CustomerDeleteDialog.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { Modal } from "react-bootstrap";
+import Table from 'react-bootstrap/Table'
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import {ModalProgressBar} from "../../../../../../_metronic/_partials/controls";
-
+import { ModalProgressBar } from "../../../../../../_metronic/_partials/controls";
 
 export function CustomerDeleteDialog({ id, show, onHide }) {
   // Customers UI Context
@@ -23,7 +23,7 @@ export function CustomerDeleteDialog({ id, show, onHide }) {
   }, [id]);
 
   // looking for loading/dispatch
-  useEffect(() => {}, [isLoading, dispatch]);
+  useEffect(() => { }, [isLoading, dispatch]);
 
 
   return (
@@ -45,6 +45,90 @@ export function CustomerDeleteDialog({ id, show, onHide }) {
           <span>Are you sure to permanently delete this customer?</span>
         )}
         {isLoading && <span>Customer is deleting...</span>}
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>From FB</th>
+              <th>From GPLUS</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Xxxxx@gmail.com</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+              <td>Table cell</td>
+            </tr>
+          </tbody>
+        </Table>
+
+
+
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Network Size</th>
+              <th>Valideted Referrels</th>
+              <th>Network Revenue</th>
+              <th>Device Class</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>
+              <td>Android</td>
+            </tr>
+          </tbody>
+        </Table>
+
+
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Geo</th>
+              <th>IP</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Us</td>
+              <td>45az4e5za4e5a</td>
+            </tr>
+          </tbody>
+        </Table>
+
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Action</th>
+              <th>Amount</th>
+              <th>Info</th>
+              <th>CreditAfter</th>
+              <th>BonusAfter</th>
+              <th>CreatedAt</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>11</td>
+              <td>bonus</td>
+              <td>0.004</td>
+              <td>dailybonus</td>
+              <td>0348</td>
+              <td>00000</td>
+              <td>22/22/15151</td>
+              
+            </tr>
+          </tbody>
+        </Table>
+
+
       </Modal.Body>
       <Modal.Footer>
         <div>
@@ -53,7 +137,7 @@ export function CustomerDeleteDialog({ id, show, onHide }) {
             onClick={onHide}
             className="btn btn-light btn-elevate"
           >
-            exit
+            Cancel
           </button>
           <> </>
         </div>
